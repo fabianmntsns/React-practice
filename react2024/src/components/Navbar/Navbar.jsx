44bdc81614250a096f3f6217ec159b6600932b15
Antony F Montesinos <afmm101294@gmail.com>
2024-06-04 16:31:36 -0300

    my first commit

---
+++ react2024/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+    return(
+        <header>
+            <h1>E Commerce</h1>
+            <section>
+                <button> Pantalones</button>
+                <button> Camisas </button>
+                <button> Zapatos </button>
+            </section>
+        </header>
+    )
+ 
+}
+export default Navbar
